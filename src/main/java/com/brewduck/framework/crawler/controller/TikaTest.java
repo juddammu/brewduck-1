9e9f42c1294854986101f73c304b3412764a9c6a
hukoru <hukoru@naver.com>
2014-06-27 18:14:18 +0900

    크롤링 샘플 추가

---
+++ src/main/java/com/brewduck/framework/crawler/controller/TikaTest.java
@@ -0,0 +1,38 @@
+package com.brewduck.framework.crawler.controller;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.html.HtmlParser;
+import org.apache.tika.sax.BodyContentHandler;
+import org.apache.tika.sax.LinkContentHandler;
+import org.apache.tika.sax.TeeContentHandler;
+import org.apache.tika.sax.ToHTMLContentHandler;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+/**
+ *
+ * @author revfactory
+ */
+public class TikaTest {
+    public static void main(String[] args) throws FileNotFoundException, SAXException, IOException, TikaException {
+        URL url = new URL("http://www.naver.com/");
+        InputStream input = url.openStream();
+        LinkContentHandler linkHandler = new LinkContentHandler();
+        ContentHandler textHandler = new BodyContentHandler();
+        ToHTMLContentHandler toHTMLHandler = new ToHTMLContentHandler();
+        TeeContentHandler teeHandler = new TeeContentHandler(linkHandler, textHandler, toHTMLHandler);
+        Metadata metadata = new Metadata();
+        ParseContext parseContext = new ParseContext();
+        HtmlParser parser = new HtmlParser();
+        parser.parse(input, teeHandler, metadata, parseContext);
+        System.out.println("title:\n" + metadata.get("title"));
+        //System.out.println("links:\n" + linkHandler.getLinks());
+        //System.out.println("text:\n" + textHandler.toString());
+        //System.out.println("html:\n" + toHTMLHandler.toString());
+    }
+}
