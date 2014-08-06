0086717dc3c9c1194ea8b27f97ffcbde6afbbf58
hukoru <hukoru@naver.com>
2014-08-06 16:13:07 +0900

    메인노출 레시피 작업

--- src/main/java/com/brewduck/web/homebrew/controller/HomebrewController.java
+++ src/main/java/com/brewduck/web/homebrew/controller/HomebrewController.java
@@ -1,20 +1,25 @@
 package com.brewduck.web.homebrew.controller;
 
 import com.brewduck.framework.security.AuthenticationUtils;
-import com.brewduck.web.domain.Account;
-import com.brewduck.web.domain.Fermentable;
-import com.brewduck.web.domain.Hop;
-import com.brewduck.web.domain.Recipe;
+import com.brewduck.web.domain.*;
 import com.brewduck.web.recipe.service.RecipeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 /**
@@ -97,6 +102,99 @@ public class HomebrewController {
         model.addAttribute("recipeDetail", recipeDetail);
 
         return "homebrew/publish";
+    }
+
+    @RequestMapping(value = "/write", method = RequestMethod.POST)
+    public String writeBoardMain(@ModelAttribute("board") Board board,
+                                 @RequestParam MultipartFile file,
+                                 @RequestParam MultipartFile coverFile,
+                                 HttpServletRequest request,
+                                 BindingResult result,
+                                 RedirectAttributes redirectAttributes) throws IOException {
+
+        int fileseq = 0;
+        int coverFileseq = 0;
+
+        HttpSession session = request.getSession();
+
+        if(coverFile.getSize() > 0){
+            FileInfo coverFileInfo = new FileInfo();
+
+            String coverFileName = coverFile.getOriginalFilename();   //파일명
+            String coverFilemime = coverFile.getContentType();        //마임 타입
+            String coverFilePath = session.getServletContext().getRealPath("/")+"/resources/upload/";   //파일 path
+            coverFile.transferTo(new File(coverFilePath+coverFileName));
+            Long coverFileSze = coverFile.getSize();
+
+            coverFileseq = commonService.selectFileSeq();
+            coverFileInfo.setFileNo(coverFileseq);
+
+            GregorianCalendar gc = new GregorianCalendar();
+            SimpleDateFormat simDate = new SimpleDateFormat("yyyyMMddHHmmss");
+            Date nowTime = gc.getTime();
+            String[] ArrFileName = null;
+
+            if( coverFileName.indexOf(".") >= 0 ) {
+                ArrFileName = coverFileName.split("\\.");
+            }
+            coverFileInfo.setSeq(coverFileseq);
+            coverFileInfo.setFilename(coverFileName);
+            coverFileInfo.setRealFilename(ArrFileName[0] + "_" + simDate.format(nowTime)+"."+ArrFileName[1]);
+            coverFileInfo.setFilesize(coverFile.getSize());
+            coverFileInfo.setFileNo(1);
+            coverFileInfo.setFilemime(coverFileName);
+
+            int fileInsertCount = commonService.insertNoticeFile(coverFileInfo);
+        }
+
+        if(file.getSize() > 0){
+
+            FileInfo fileInfo = new FileInfo();
+
+            String fileName = file.getOriginalFilename();   //파일명
+            String filemime = file.getContentType();        //마임 타입
+            String filePath = session.getServletContext().getRealPath("/")+"/resources/upload/";   //파일 path
+            file.transferTo(new File(filePath+fileName));
+            Long fileSze = file.getSize();
+
+            fileseq = commonService.selectFileSeq();
+            fileInfo.setFileNo(fileseq);
+
+            GregorianCalendar gc = new GregorianCalendar();
+            SimpleDateFormat simDate = new SimpleDateFormat("yyyyMMddHHmmss");
+            Date nowTime = gc.getTime();
+            String[] ArrFileName = null;
+
+            if( fileName.indexOf(".") >= 0 ) {
+                ArrFileName = fileName.split("\\.");
+            }
+            fileInfo.setSeq(fileseq);
+            fileInfo.setFilename(fileName);
+            fileInfo.setRealFilename(ArrFileName[0] + "_" + simDate.format(nowTime)+"."+ArrFileName[1]);
+            fileInfo.setFilesize(file.getSize());
+            fileInfo.setFileNo(1);
+            fileInfo.setFilemime(filemime);
+
+            int fileInsertCount = commonService.insertNoticeFile(fileInfo);
+        }
+
+        Account account = AuthenticationUtils.getUser();
+        String name = account.getName();
+
+        board.setAtchFileId(fileseq+"");
+        board.setBbsId(board.getBbsId());
+        board.setAtchCoverFileId(coverFileseq+"");
+        board.setNttNo(1);
+        board.setSortOrder(1);
+        board.setUseAt("Y");
+        board.setAnswerAt("Y");
+        board.setInsertId(name);
+
+        int insertCount = boardService.writeBoardArticle(board);
+
+        logger.info(" @@@ " + board.getBbsNm());
+
+        return "community/write";
     }
 
     /**
