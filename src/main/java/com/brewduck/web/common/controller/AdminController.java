9834b905c80fa03534e180277132323ffdc9d033
juddammu <juddammus@gmail.com>
2014-08-11 11:48:35 +0900

    14.08.11 차준영 회원 정보 페이지

---
+++ src/main/java/com/brewduck/web/common/controller/AdminController.java
@@ -0,0 +1,67 @@
+package com.brewduck.web.common.controller;
+
+import com.brewduck.framework.security.AuthenticationUtils;
+import com.brewduck.web.common.service.AdminService;
+import com.brewduck.web.common.service.BoardService;
+import com.brewduck.web.domain.Account;
+import com.brewduck.web.domain.Admin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/admin")
+public class AdminController {
+
+    private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
+
+    @Autowired
+    private AdminService adminService;
+
+    /**
+     * <pre>
+     * 게시판 메인
+     * </pre>
+     *
+     * @param model Model
+     * @return 게시판 메인
+     */
+    @RequestMapping(value = "/main", method = RequestMethod.GET)
+    public String admindMain(Model model, Admin admin) {
+        logger.info("Admin Main");
+
+        Account account = AuthenticationUtils.getUser();
+        model.addAttribute("account", account);
+
+        return "admin/main";
+    }
+
+    /**
+     * <pre>
+     * 자유게시판 리스트.
+     * </pre>
+     *
+     * @param model Model
+     * @return 자유게시판 리스트
+     */
+    @RequestMapping(value = "/userList", method = RequestMethod.GET)
+    public List<Admin> selectUserList(Model model, Admin admin) {
+        logger.info("User List");
+
+        List<Admin> list = adminService.selectUserList(admin);
+        logger.info("User List : {}", list.size());
+        model.addAttribute("list", list);
+
+        return list;
+    }
+}
+
+
---
+++ src/main/webapp/WEB-INF/views/admin/userList.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+
+<c:forEach items="${list}" var="list">
+    <tr onclick = "">
+        <td>${list.id}</td>
+        <td>${list.name}</td>
+        <td>${list.email}</td>
+        <td>${list.authority}</td>
+        <td>${list.activation}</td>
+        <td>${list.insertDate}</td>
+    </tr>
+</c:forEach>
+
+<content tag="local_script">
+    <script>
+        $(document).ready(function() {
+            var selectedItems=0;
+        });
+
+    </script>
+</content>
